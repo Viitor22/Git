6a9afee304beab7c79da0fe9f51c91666eff64ea
Caio Simplicio <caio1simplicio@gmail.com>
2024-03-23 20:38:55 -0300

    remove wrong commit

--- base_exercicio_css_in_js-main/src/components/Cabecalho/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Cabecalho = styled.h1`
-    background-color: var(--cor-secundaria);
-    color: var(--cor-principal);
-    text-align: center;
-    padding: 24px 0;
-`
